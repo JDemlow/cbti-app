03f8ff80f16e47cf00ca2830c7884baed85d2362
JDemlow <jdemlowdev@gmail.com>
2025-04-15 21:56:33 -0600

    fixing navigation

---
+++ src/app/(app)/relaxation/maetadata.ts
@@ -0,0 +1,7 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Relaxation Techniques | RealSleep",
+  description:
+    "Guided relaxation techniques to help you prepare for sleep and reduce anxiety",
+};
